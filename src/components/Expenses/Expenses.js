d1e2985af363330fb2c5f54e431fad08932e30e9
its-kumar-yash <yash-kumar.ug21@nsut.ac.in>
2023-02-07 00:51:53 +0530

    Edit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "./ExpensesFilter";
